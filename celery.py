52343eb1ea09ff941dcbc63efe73325ff9112fe8
ouril <ourilkregg@gmail.com>
2017-09-22 18:43:15 +0300

    start project

---
+++ celery.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery
+
+
+app = Celery(
+    'celery_first',
+    broker='amqp://',
+    backend='amqp://',
+    include=['celery_first.tasks']
+)
+
+app.conf.update(
+    result_expires=3600,
+)
+
+if __name__ = '__main__':
+    app.start()
